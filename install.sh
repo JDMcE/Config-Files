ead13cf52937ec1eebbef88e354e68ba77c97fc5
John M <stealthee.camel@gmail.com>
2024-05-03 19:28:40 +0100

    Add zsh plugins

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "Installing plugins ..."
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 echo "Installing config files ..."
 
 ln -s $HOME/Config-Files/.zshrc $HOME/.zshrc
