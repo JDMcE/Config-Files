61c23569d6edfea1e81e1afce232beee5719e297
jdmce <stealthee.camel@gmail.com>
2025-01-28 10:33:46 +0000

    added dracula theme for micro

--- install.sh
+++ install.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-echo "Installing plugins ..."
+echo "Installing OhMyZsh plugins ..."
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+echo "Installing micro theme ..."
+
+git clone https://github.com/dracula/micro.git
+
+
+if [ ! -d "$HOME/.config/micro/colorschemes/" ]; then
+  mkdir $HOME/.config/micro/colorschemes/dracula.micro
+fi
+
+ln -s $HOME/Config-Files/micro/dracula.micro $HOME/.config/micro/colorschemes/dracula.micro
+
 
 echo "Installing config files ..."
 
 ln -s $HOME/Config-Files/.zshrc $HOME/.zshrc
 ln -s $HOME/Config-Files/.tmux.conf $HOME/.tmux.conf
 ln -s $HOME/Config-Files/.vimrc $HOME/.vimrc
+
 
 echo "Done"
