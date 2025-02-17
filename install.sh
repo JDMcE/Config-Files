c0293bb9859c42f365f929af6fa2798aaf92cd70
John M <stealthee.camel@gmail.com>
2025-02-17 23:21:03 +0000

    Add kali option to install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 installmissing=false
-while getopts ":i" opt; do
+onKali=false
+while getopts ":ikh" opt; do
   case $opt in
     i) installmissing=true ;;
+    k) onKali=true ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
       exit 1
@@ -39,15 +41,36 @@ echo "Micro setup ..."
 if [ ! -d "$HOME/.config/micro/colorschemes/" ]; then
   mkdir -p $HOME/.config/micro/colorschemes/
 fi
-ln -s $HOME/Config-Files/micro/dracula.micro $HOME/.config/micro/colorschemes/dracula.micro
-ln -s $HOME/Config-Files/micro/bindings.json $HOME/.config/micro/bindings.json
-ln -s $HOME/Config-Files/micro/settings.json $HOME/.config/micro/settings.json
+ln -sf $HOME/Config-Files/micro/dracula.micro $HOME/.config/micro/colorschemes/dracula.micro
+ln -sf $HOME/Config-Files/micro/bindings.json $HOME/.config/micro/bindings.json
+ln -sf $HOME/Config-Files/micro/settings.json $HOME/.config/micro/settings.json
 
 # Link files
 echo "Installing config files ..."
 ln -s $HOME/Config-Files/.zshrc $HOME/.zshrc
-ln -s $HOME/Config-Files/.tmux.conf $HOME/.tmux.conf
-ln -s $HOME/Config-Files/.vimrc $HOME/.vimrc
+ln -sf $HOME/Config-Files/.tmux.conf $HOME/.tmux.conf
+ln -sf $HOME/Config-Files/.vimrc $HOME/.vimrc
+
+# Use kali zsh since OhMyZsh usually isnt installed
+if $onKali; then
+	echo """
+alias tns="tmux new -s"
+alias tks="tmux kill-session -t"
+alias tksv="tmux kill-server"
+alias tls="tmux list-sessions"
+alias tas="tmux attach-session -t" 
+alias mi="micro"
+alias fz="fzf"
+#alias xclip="xclip -sel clip"
+
+# Set up fzf key bindings and fuzzy completion
+export FZF_DEFAULT_OPTS='--height 40% --layout reverse --border top --style minimal --preview "fzf-preview.sh {}" --bind "focus:transform-header:file --brief {}"'
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+
+export MICRO_TRUECOLOR=1
+""" >> $HOME/.zshrc
+fi
 
 
 echo -e "\n\nTODO:"
